4df2c3aeb1760395114800cb257b0c12e573a294
Franco Ansaldi <106043508+francoansaldi7@users.noreply.github.com>
2023-06-24 14:04:52 -0300

    Primer commit, cargando proyecto

---
+++ src/test/java/com/example/FINALANSALDIFRANCO/FinalAnsaldiFrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FINALANSALDIFRANCO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAnsaldiFrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
