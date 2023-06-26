10556c0aebf26b61de7560aae3ef168eb0c0e202
Franco Ansaldi <106043508+francoansaldi7@users.noreply.github.com>
2023-06-26 15:49:18 -0300

    adding controllers, service, application properties

--- src/main/java/com/controller/OdontologoController.java
+++ src/main/java/com/controller/OdontologoController.java
@@ -1,4 +1,58 @@
 package com.controller;
 
+import com.entity.Odontologo;
+import com.service.OdontologoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/odontologos")
 public class OdontologoController {
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @PostMapping
+    public ResponseEntity<Odontologo> guardarOdontologo (@RequestBody Odontologo odontologo){
+        return ResponseEntity.ok(odontologoService.guardarOdontologo(odontologo));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Odontologo>> listarOdontologos(){
+        return ResponseEntity.ok(odontologoService.listarOdontologos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Odontologo> buscarOdontologoPorId(@PathVariable Long id){
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarOdontologo(id);
+        if(odontologoBuscado.isPresent()){
+            return ResponseEntity.ok(odontologoBuscado.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarOdontologo(@PathVariable Long id){
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarOdontologo(id);
+        if (odontologoBuscado.isPresent()){
+            odontologoService.eliminarOdontologo(id);
+            return ResponseEntity.ok("Se elimino el odontologo con ID: " + id + " correctamente");
+        } else {
+            return ResponseEntity.badRequest().body("No se encontró el odontologo con ID: " + id);
+        }
+    }
+
+    @GetMapping("/matricula/{matricula}")
+    public ResponseEntity<Odontologo> buscarPorMatricula(@PathVariable String matricula){
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscarPorMatricula(matricula);
+        if (odontologoBuscado.isPresent()){
+            return ResponseEntity.ok(odontologoBuscado.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/controller/TurnoController.java
+++ src/main/java/com/controller/TurnoController.java
@@ -1,4 +1,71 @@
 package com.controller;
 
+import com.entity.Paciente;
+import com.entity.Turno;
+import com.service.OdontologoService;
+import com.service.PacienteService;
+import com.service.TurnoService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/turnos")
 public class TurnoController {
+    @Autowired
+    private TurnoService turnoService;
+    @Autowired
+    private PacienteService pacienteService;
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @PostMapping
+    public ResponseEntity<Turno> guardarTurno (@RequestBody Turno turno){
+        if (odontologoService.buscarOdontologo(turno.getOdontologo().getId()).isPresent() && pacienteService.buscarPaciente(turno.getPaciente().getId()).isPresent()){
+            return ResponseEntity.ok(turnoService.guardarTurno(turno));
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Turno>> listarTurnos (){
+            return ResponseEntity.ok(turnoService.listarTurnos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Turno> buscarPorId(@PathVariable Long id){
+        Optional<Turno> turnoBuscado = turnoService.buscarPorId(id);
+        if (turnoBuscado.isPresent()){
+            return ResponseEntity.ok(turnoBuscado.get());
+        } else {
+                return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarTurno (@PathVariable Long id){
+        Optional<Turno> turnoBuscado = turnoService.buscarPorId(id);
+        if (turnoBuscado.isPresent()){
+            turnoService.eliminarTurno(id);
+            return ResponseEntity.ok("Se eliminó correctamente el turno con ID: " + id);
+        } else {
+            return ResponseEntity.badRequest().body("No existe el turno con ID: " + id);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<String> actualizarTurno(@RequestBody Turno turno){
+        Optional<Turno> turnoBuscado = turnoService.buscarPorId(turno.getId());
+        if (turnoBuscado.isPresent()){
+            turnoService.actualizarTurno(turno);
+            return ResponseEntity.ok("Se actualizó correctamente el turno con ID: " + turno.getId());
+        } else {
+            return ResponseEntity.badRequest().body("No existe o no se puede actualizar el turno con ID: " + turno.getId());
+        }
+    }
 }
